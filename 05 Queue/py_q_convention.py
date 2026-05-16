33d672a803e41d8b84823c9adad0d162fd90829a
b-4her <bahercsjourney@gmail.com>
2026-05-16 17:21:04 -0500

    update py q convention

--- 05 Queue/py_q_convention.py
+++ 05 Queue/py_q_convention.py
@@ -14,6 +14,7 @@ print("Peek:", q[0]) # peek is the front (first added item)
 # it is pop left because the new appended ones are added to the right,
 # and in q we want to dequeue the old elements (first in first out)
 
+# head; q[0] -> left most element
 print(q.popleft())  # dequeue  O(1)
 print(q) 
 print(q.popleft())
